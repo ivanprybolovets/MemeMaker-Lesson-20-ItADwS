e059a6292b83afb39714c0bf14350e8cac029b8c
Ivan Prybolovetz <iprybolovetz@gmail.com>
2020-04-04 12:34:03 +0300

    Initial Commit

---
+++ MemeMaker Lesson 20 ItADwS/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MemeMaker Lesson 20 ItADwS
+//
+//  Created by Ivan Prybolovetz on 4/4/20.
+//  Copyright © 2020 Ivan Prybolovetz. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
