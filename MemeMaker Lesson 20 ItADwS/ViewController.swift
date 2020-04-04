e084f7d4a34a6c00b595c04bccf515726b039c6b
Ivan Prybolovetz <iprybolovetz@gmail.com>
2020-04-04 13:45:00 +0300

    Add iteration tTopSegmentsControl, BottomSegmentsControl

--- MemeMaker Lesson 20 ItADwS/ViewController.swift
+++ MemeMaker Lesson 20 ItADwS/ViewController.swift
@@ -16,10 +16,46 @@ class ViewController: UIViewController {
     @IBOutlet weak var bottomLabel: UILabel!
     @IBOutlet weak var bottomSegmentControl: UISegmentedControl!
     
+    var topChoices = [CaptionOption]()
+    var bottomChoices: [CaptionOption] = []
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        configureTopSegmentControl()
+        configureBottomSegmentControl()
+    }
+    
+    func configureTopSegmentControl() {
+        topCaptionSegmentControl.removeAllSegments()
+        let option1 = CaptionOption(emoji: "🕶", text: "You know what’s cool?", imageName: "imageName")
+        let option2 = CaptionOption(emoji: "💥", text: "You know what makes me mad?", imageName: "imageName")
+        let option3 = CaptionOption(emoji: "💕", text: "You know what I love?", imageName: "imageName")
+        topChoices = [option1, option2, option3]
+        
+        for choice in topChoices {
+            topCaptionSegmentControl.insertSegment(withTitle: choice.emoji, at: topChoices.count, animated: false)
+        }
+    }
+    
+    func configureBottomSegmentControl() {
+        bottomSegmentControl.removeAllSegments()
+        let option1 = CaptionOption(emoji: "🐱", text: "Cats wearing hats", imageName: "")
+        let option2 = CaptionOption(emoji: "🐕", text: "Dogs carrying logs", imageName: "")
+        let option3 = CaptionOption(emoji: "🙉", text: "Monkeys being funky", imageName: "")
+        bottomChoices = [option1, option2, option3]
+        
+        for choice in bottomChoices {
+            bottomSegmentControl.insertSegment(withTitle: choice.emoji, at: topChoices.count, animated: false)
+        }
     }
     
     
+    
+    @IBAction func topSegmentValueChanged(_ sender: UISegmentedControl) {
+        
+    }
+    @IBAction func bottomSegmentValueChanged(_ sender: UISegmentedControl) {
+        
+    }
 }
-
